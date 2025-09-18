70860fcb53b07fc5fe792723bf7241c0c1c5ce5c
Steven <ungsteven6@gmail.com>
2025-09-18 12:02:51 +0200

    mongodb dotenv

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,31 @@
+
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.MONGODB_URI;
+const dbName = process.env.MONGODB_DB;
+
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Backend is running!');
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+app.get('/', async (req, res) => {
+  try {
+    await client.connect();
+    await client.db(dbName).command({ ping: 1 });
+    res.send('Backend is running and connected to MongoDB!');
+  } catch (err) {
+    res.status(500).send('Error connecting to MongoDB');
+  }
 });
 
 app.listen(PORT, () => {
